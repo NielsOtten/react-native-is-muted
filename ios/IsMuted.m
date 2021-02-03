95fcd752ac43db0c4c5da3e4c3a37b95e871f050
nylsoo <tnnielstn@gmail.com>
2021-02-03 15:36:09 +0100

    feat: add error message when using ios simulator

--- ios/IsMuted.m
+++ ios/IsMuted.m
@@ -14,6 +14,10 @@ RCT_REMAP_METHOD(isMuted,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
+    #if TARGET_IPHONE_SIMULATOR
+        reject(@"isMuted", @"The simulator currently doesn't support react-native-is-muted.", [NSError errorWithDomain:@"SimulatorNotSupported" code:0 userInfo:@{}]);
+    #endif
+
     self.muteChecker = [[MuteChecker alloc] initWithCompletionBlk:^(BOOL muted) {
         resolve(muted ? @TRUE : @FALSE);
     }];
